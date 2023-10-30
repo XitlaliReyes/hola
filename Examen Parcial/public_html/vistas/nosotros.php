0037ce7e285e55dfa4c9fb23aa7378f9d0a1605d
surebria <ppollux.velas@gmail.com>
2023-10-29 22:02:32 -0600

    jejev

--- Examen Parcial/public_html/vistas/nosotros.php
+++ Examen Parcial/public_html/vistas/nosotros.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/estylos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>¿Qué hacemos?</title>
     
 </head>
@@ -22,25 +23,93 @@
             <div class="col3 cards">
                 
                 <div class="card3 card1">
-                    <h5 id="ttl5">Innovación Continua</h5>
-                    <p id="txt5"> Buscar constantemente nuevas formas de resolver problemas y adoptar tecnologías avanzadas.</p>
+                    <div class="overlay">
+                        <h5 id="ttl5">Innovación Continua</h5>
+                        <p id="txt5"> Buscar constantemente nuevas formas de resolver problemas y adoptar tecnologías avanzadas.</p>
+                    </div>
                 </div>
                 <div class="card3 card2">
-                    <h5 id="ttl5">Calidad del Software</h5>
-                    <p id="txt5"> Desarrollar software confiable, eficiente y sin errores.</p>
+                    <div class="overlay">
+                        <h5 id="ttl5">Calidad del Software</h5>
+                        <p id="txt5"> Desarrollar software confiable, eficiente y sin errores.</p>
+                    </div>
                 </div>
                 <div class="card3 card4">
-                    <h5 id="ttl5">Atención al Cliente</h5>
-                    <p id="txt5">Poner al cliente en el centro, comunicarse efectivamente y satisfacer sus necesidades.</p>
+                    <div class="overlay">
+                        <h5 id="ttl5">Atención al Cliente</h5>
+                        <p id="txt5">Poner al cliente en el centro, comunicarse efectivamente y satisfacer sus necesidades.</p>
+                    </div>
                 </div>
                 <div class="card3 card5">
-                    <h5 id="ttl5">Integridad y Ética</h5>
-                    <p id="txt5"> Actuar de manera ética, proteger la privacidad y cumplir con las leyes y regulaciones.</p>
+                    <div class="overlay">
+                        <h5 id="ttl5">Integridad y Ética</h5>
+                        <p id="txt5"> Actuar de manera ética, proteger la privacidad y cumplir con las leyes y regulaciones.</p>
+                    </div>
                 </div>
 
             </div>
         </div>
     </div>
+
+    <section class="project">
+    <h3 class="text-center" style="font-weight: bold; letter-spacing: 2px; font-size: 35px;">
+        Proyectos Destacados
+        <u><i class="fas fa-star"></i></u>
+    </h3>
+
+        <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="image-overlay-3">
+                    <img src="../img/inovacion.png" class="img-fluid" alt="Imagen 3">
+                    <div class="overlay-3">
+                        <h3>ConstrucPro</h3>
+                        <p>"ConstrucPro" es una plataforma de software diseñada para la gestión de proyectos de construcción. 
+                            Ayuda a las empresas de construcción a planificar, programar, asignar recursos y realizar un seguimiento detallado de sus proyectos, desde la fase de diseño hasta la finalización de la construcción.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container ml-md-auto">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="image-overlay-2">
+                    <img src="../img/inovacion.png" class="img-fluid" alt="Imagen 3">
+                    <div class="overlay-2">
+                        <h3>NoteMaster</h3>
+                        <p>"NoteMaster" es una aplicación de software que ayuda a los usuarios a organizar sus notas, listas de tareas y recordatorios. 
+                            Los usuarios pueden crear, editar y organizar sus notas de manera eficaz, sincronizándolas en múltiples dispositivos para acceder a ellas en cualquier momento.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <!-- Imagen a la izquierda, texto a la derecha -->
+                <div class="image-overlay-3">
+                    <img src="../img/inovacion.png" class="img-fluid" alt="Imagen 3">
+                    
+                    <div class="overlay-3">
+                       
+                        <h3>BookMaster</h3>
+                        <p>"LibroMaster" es una solución avanzada de software diseñada para simplificar la gestión de bibliotecas de todo tipo. 
+                            Este sistema de gestión de biblioteca permite a bibliotecarios y administradores llevar un control completo de la colección de libros, los usuarios y los préstamos. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                
+            </div>
+            <div class="col-md-6"></div>
+            <div class="col-md-6"></div>
+        </div>
+    </div>
+    </section>
     <?php
         include "../footer.html";    
     ?>
